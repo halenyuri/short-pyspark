ca4a58d8cac808e1ff9998c149ff079f28f4276a
Hálen Yuri <halenyuri@users.noreply.github.com>
2019-12-16 18:34:20 +0000

    Add files via upload
    need to append the columns

--- convertFiles.py
+++ convertFiles.py
@@ -4,6 +4,7 @@ import threading
 import ast
 from pyspark import SparkSession
 import pyspark.sql
+from pyspark.sql.types import *
 
 sc = SparkSession.builder \
     .appName("NDOD - Convert files") \
@@ -14,16 +15,27 @@ sc = SparkSession.builder \
 
 def createSchema (fields, dftext):
     tbfields = [StructField(field_name, StringType(), True) for field_name in fields]
-    content = []
-    value = ""
+    content = {}
 
-    for l in open("/tmp/NDOD_REST_TIERS_DELTA_30007_RCT_191204-101015.txt.txt", "r"):
-        content.append(value)
+    for l in open("C:\Users\oliveirahy\Documents\NDOD\NDOD_REST_TIERS_DELTA_30007_RCT_191204-101015.txt.txt", "r"):
         value = ""
-        for i in fields:
+        for i in list(fields):
             sp = fields[i].split(",")[0]
             ep = fields[i].split(",")[1]
-            value = l[sp:ep] + ","
+            value += l[int(sp):int(ep)] + ","
+        content.append(value)
+
+    for i in list(fields):
+        value = []
+        sp = fields[i].split(",")[0]
+        ep = fields[i].split(",")[1]
+        for l in open("hdfs://tmp/NDOD_REST_TIERS_DELTA_30007_RCT_191204-101015.txt.txt", "r"):
+            value.append(l[int(sp):int(ep)])
+        column_name = StructType([StructField(str(i), StringType(), True)])
+        rdd = sc.parallelize(value).map(lambda x: Row(x))
+        dfcol = sqlContext.createDataFrame(rdd, column_name)
+        df = df.withColumn(lit(i), dfcol[str(i)])
+
 
 "/tmp/NDOD_REST_TIERS_DELTA_30007_RCT_191204-101015.txt.txt"
 def main ():
@@ -34,6 +46,8 @@ def main ():
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
+    props = {}
+
     #Read the argument file and segregate the properties.
     with open(sys.argv[1], "r") as f:
         for line in f:
@@ -43,6 +57,6 @@ def main ():
     # Use the field properties to read the input file
     fields = ast.literal_eval(props["fields"])
 
-    df = sc.textFile(sys.argv[2])
-
-    tbfields = [StructField(field_name, StringType(), True) for field_name in fields]
+    #tbfields = [StructField(field_name, StringType(), True) for field_name in fields]
+    schema = StructType([])
+    df = sqlContext.createDataFrame(sc.emptyRDD(), schema)
